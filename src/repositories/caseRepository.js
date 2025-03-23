8ee2076bdda936b83b74baea4f556b5a69556e3b
Natalia <nataliabento97@gmail.com>
2025-03-23 09:33:05 -0300

    feat: cria repository de caso

---
+++ src/repositories/caseRepository.js
@@ -0,0 +1,45 @@
+const Case = require("../models/caseModel");
+
+//cria um novo caso
+const createCase = async (data) => {
+    const newCase = new Case(data);
+    return await newCase.save();
+};
+
+//busca pelo id 
+const getCaseById = async (id) => {
+    return await Case.findById(id); // aqui eu to passando o parametro pelo id, porque fica mais facil de entender, ao inves de data
+};
+
+//lista todos os casos com paginação
+const getAllCases = async (offSet, limit) => {
+    return await Case.find()
+    .skip(offSet)
+    .limit(limit);
+};
+
+//lista casos por status andamento...finalizado...
+const getCasesByStatus = async (status, offSet, limit) => {
+    return await Case.find()
+    .where("status")
+    .equals(status)
+    .skip(offSet)
+    .limit(limit);
+};
+
+//atualiza o status de um caso e define a data de fechamento se for finalizado
+const updateCaseStatus = async (id, newStatus, closedAt = null) => {
+    const updateData = { status: newStatus};
+    if (closedAt) {
+        updateData.closedAt = closedAt;
+    }
+    return await Case.findByIdAndUpdate(id, updateData, {new: true});
+};
+
+module.exports = {
+    createCase,
+    getCaseById,
+    getAllCases,
+    getCasesByStatus,
+    updateCaseStatus,
+  };
